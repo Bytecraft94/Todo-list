86e9c1ed1e81c6f10f712328c2b03ca5b1c4f2cc
bytecraft <mrizwwan.bytecraft@gmail.com>
2025-02-10 21:01:04 +0500

    done

---
+++ data/todos.js
@@ -0,0 +1,48 @@
+export const data = [
+  {
+    "id": "1",
+    "title": "Buy groceries",
+    "completed": false
+  },
+  {
+    "id": "2",
+    "title": "Finish React Native project",
+    "completed": false
+  },
+  {
+    "id": "3",
+    "title": "Workout",
+    "completed": true
+  },
+  {
+    "id": "4",
+    "title": "Read a book",
+    "completed": false
+  },
+  {
+    "id": "5",
+    "title": "Call Mom",
+    "completed": true
+  },
+  {
+    "id": "6",
+    "title": "Clean the house",
+    "completed": false
+  },
+  {
+    "id": "7",
+    "title": "Finish the report",
+    "completed": true
+  },
+  {
+    "id": "8",
+    "title": "Take a walk",
+    "completed": false
+  },
+  {
+    "id": "9",
+    "title": "Plan weekend trip",
+    "completed": false
+  }
+ 
+];
